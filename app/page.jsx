e71203c115f70312640a99d51c82227b67aefdb8
Hedayatullah Sarwary <hedayatullah.hedayat.sarwary@gmail.com>
2026-08-17 15:27:12 +0430

    Property Pulse Navbar is Modified

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-const HomePAge = () => {
+const HomePage = () => {
   return (
     <div>
       <h1 className="text-3xl">
@@ -12,4 +12,4 @@ const HomePAge = () => {
   )
 }
 
-export default HomePAge
+export default HomePage
